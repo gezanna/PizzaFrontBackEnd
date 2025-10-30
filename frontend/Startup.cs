dc965f3897da591092fb05df2f4fa6ebdb6619cb
gezanna <yoni.natnna@gmail.com>
2025-10-30 09:06:55 +0100

    1st commit to branch

--- frontend/Startup.cs
+++ frontend/Startup.cs
@@ -48,7 +48,7 @@ namespace frontend
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
-                app.UseHsts();
+                
             }
 
             // some additional test
